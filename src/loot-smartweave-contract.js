bedc075e984d4f05bbeb41a35f43563e364493e6
Alex Suvorov <Suvorovalex1997@gmail.com>
2021-09-22 14:06:26 +0200

    Local storage cache (#2)
    * local storage cache conencted, but there is error with mem quota
    
    * cache fixed
    
    * definition cache only ipmplemented
    
    * connected http cache
    
    * redstone-smartweave updated
    
    Co-authored-by: ppedziwiatr <peter@redstone.finance>

--- src/loot-smartweave-contract.js
+++ src/loot-smartweave-contract.js
@@ -1,40 +1,52 @@
-import Arweave from 'arweave'
+import Arweave from 'arweave';
 import {
+  CacheableContractInteractionsLoader,
+  CacheableStateEvaluator,
+  ContractInteractionsLoader,
   LoggerFactory,
-  ContractDefinitionLoader,
-  // ContractInteractionsLoader,
-  // CacheableContractInteractionsLoader,
-  SmartWeaveWebFactory,
-} from 'redstone-smartweave'
-import LocalStorageCache from "@/cache/cache"
-// import LocalStorageBlockHeightCache from "@/cache/block-height-cache"
-import deployedContracts from './deployed-contracts.json'
+  RemoteBlockHeightCache,
+  SmartWeaveWebFactory
+} from 'redstone-smartweave';
+import deployedContracts from './deployed-contracts.json';
+
+
+// const CACHE_API_URL = 'https://dnmt9ldcqj0gz.cloudfront.net/cache'
+// const CACHE_API_URL = 'http://localhost:3000/cache'
+// const CACHE_API_URL = 'https://duei9u4otl5h4.cloudfront.net/cache'
+// const CACHE_API_URL = 'http://smartweave-cache-express-dev.eu-north-1.elasticbeanstalk.com/cache'
+const CACHE_API_URL = 'https://smart-cache.herokuapp.com/cache'
 
 // Set up Arweave client
 export const arweave = Arweave.init({
-  host: 'arweave.net',
+  host: 'dh48zl0solow5.cloudfront.net',
   port: 443,
   protocol: 'https',
 })
 
 // Set up SmartWeave client
-LoggerFactory.INST.logLevel('silly')
+LoggerFactory.INST.logLevel("debug");
 
-const contractDefinitionLoader = new ContractDefinitionLoader(arweave, new LocalStorageCache("_REDSTONE_LOOT_APP_"))
-// const contractInteractionsLoader = new CacheableContractInteractionsLoader(
-//   new ContractInteractionsLoader(arweave),
-//   new LocalStorageBlockHeightCache("_REDSTONE_LOOT_APP_INTERACTIONS_")
-// )
-const smartweave = SmartWeaveWebFactory.memCachedBased(arweave)
-  .setDefinitionLoader(contractDefinitionLoader)
-  // .setInteractionsLoader(contractInteractionsLoader)
+const contractInteractionsLoader = new CacheableContractInteractionsLoader(
+  new ContractInteractionsLoader(arweave),
+  new RemoteBlockHeightCache('INTERACTIONS', CACHE_API_URL)
+)
+const cacheableStateEvaluator = new CacheableStateEvaluator(
+  arweave,
+  new RemoteBlockHeightCache("STATE", CACHE_API_URL)
+);
+
+const smartweave = SmartWeaveWebFactory
+  .memCachedBased(arweave)
+  .setInteractionsLoader(contractInteractionsLoader)
+  .setStateEvaluator(cacheableStateEvaluator)
   .build();
-
-// const smartweave = SmartWeaveWebFactory.memCached(arweave)
 
 // Interacting with the contract
 const contract = smartweave
   .contract(deployedContracts.loot)
   .connect('use_wallet')
+  .setEvaluationOptions({
+    waitForConfirmation: true,
+  })
 
 export default contract
