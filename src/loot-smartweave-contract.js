449dd104d4f8a47e5822d548504f88c3db4f66cb
hatskier <suvorovalex1997@gmail.com>
2021-09-22 02:39:29 +0200

    connected http cache

--- src/loot-smartweave-contract.js
+++ src/loot-smartweave-contract.js
@@ -1,14 +1,25 @@
 import Arweave from 'arweave'
 import {
   LoggerFactory,
-  ContractDefinitionLoader,
-  // ContractInteractionsLoader,
-  // CacheableContractInteractionsLoader,
+  // ContractDefinitionLoader,
+  RemoteBlockHeightCache,
+  CacheableStateEvaluator,
+  ContractInteractionsLoader,
+  CacheableContractInteractionsLoader,
   SmartWeaveWebFactory,
+  // LocalStorageBlockHeightCache,
+  // LocalStorageCache,
 } from 'redstone-smartweave'
-import LocalStorageCache from "@/cache/cache"
-// import LocalStorageBlockHeightCache from "@/cache/block-height-cache"
+// import LocalStorageCache from "@/cache/cache"
+import LocalStorageBlockHeightCache from '@/cache/block-height-cache'
 import deployedContracts from './deployed-contracts.json'
+
+
+// const CACHE_API_URL = 'https://dnmt9ldcqj0gz.cloudfront.net/cache'
+// const CACHE_API_URL = 'http://localhost:3000/cache'
+// const CACHE_API_URL = 'https://duei9u4otl5h4.cloudfront.net/cache'
+// const CACHE_API_URL = 'http://smartweave-cache-express-dev.eu-north-1.elasticbeanstalk.com/cache'
+const CACHE_API_URL = 'https://smart-cache.herokuapp.com/cache'
 
 // Set up Arweave client
 export const arweave = Arweave.init({
@@ -20,14 +31,25 @@ export const arweave = Arweave.init({
 // Set up SmartWeave client
 LoggerFactory.INST.logLevel('silly')
 
-const contractDefinitionLoader = new ContractDefinitionLoader(arweave, new LocalStorageCache("_REDSTONE_LOOT_APP_"))
-// const contractInteractionsLoader = new CacheableContractInteractionsLoader(
-//   new ContractInteractionsLoader(arweave),
-//   new LocalStorageBlockHeightCache("_REDSTONE_LOOT_APP_INTERACTIONS_")
+// const contractDefinitionLoader = new ContractDefinitionLoader(
+//   arweave,
+//   new LocalStorageCache("_REDSTONE_LOOT_APP_")
 // )
-const smartweave = SmartWeaveWebFactory.memCachedBased(arweave)
-  .setDefinitionLoader(contractDefinitionLoader)
-  // .setInteractionsLoader(contractInteractionsLoader)
+const contractInteractionsLoader = new CacheableContractInteractionsLoader(
+  new ContractInteractionsLoader(arweave),
+  new RemoteBlockHeightCache('INTERACTIONS', CACHE_API_URL)
+)
+const cacheableStateEvaluator = new CacheableStateEvaluator(
+  arweave,
+  new RemoteBlockHeightCache('STATE', CACHE_API_URL)
+  // new LocalStorageBlockHeightCache('_REDSTONE_APP_STATE_', 2, false, 100)
+)
+
+const smartweave = SmartWeaveWebFactory
+  .memCachedBased(arweave)
+  // .setDefinitionLoader(contractDefinitionLoader)
+  .setInteractionsLoader(contractInteractionsLoader)
+  // .setStateEvaluator(cacheableStateEvaluator)
   .build();
 
 // const smartweave = SmartWeaveWebFactory.memCached(arweave)
