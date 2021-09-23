6572e4fbb2c54813e4afa2de8f9e9e5fdd07b32f
ppedziwiatr <peter@redstone.finance>
2021-09-23 18:38:18 +0200

    state cache update only after full evaluation

--- src/loot-smartweave-contract.js
+++ src/loot-smartweave-contract.js
@@ -14,22 +14,22 @@ import deployedContracts from './deployed-contracts.json';
 // const CACHE_API_URL = 'http://localhost:3000/cache'
 // const CACHE_API_URL = 'https://duei9u4otl5h4.cloudfront.net/cache'
 // const CACHE_API_URL = 'http://smartweave-cache-express-dev.eu-north-1.elasticbeanstalk.com/cache'
-const CACHE_API_URL = 'https://smart-cache.herokuapp.com/cache'
+const CACHE_API_URL = 'https://smart-cache.herokuapp.com/cache';
 
 // Set up Arweave client
 export const arweave = Arweave.init({
-  host: 'dh48zl0solow5.cloudfront.net',
+  host: "dh48zl0solow5.cloudfront.net",
   port: 443,
-  protocol: 'https',
-})
+  protocol: "https",
+}); 
 
 // Set up SmartWeave client
 LoggerFactory.INST.logLevel("debug");
 
 const contractInteractionsLoader = new CacheableContractInteractionsLoader(
   new ContractInteractionsLoader(arweave),
-  new RemoteBlockHeightCache('INTERACTIONS', CACHE_API_URL)
-)
+  new RemoteBlockHeightCache("INTERACTIONS", CACHE_API_URL)
+);
 const cacheableStateEvaluator = new CacheableStateEvaluator(
   arweave,
   new RemoteBlockHeightCache("STATE", CACHE_API_URL)
@@ -44,9 +44,10 @@ const smartweave = SmartWeaveWebFactory
 // Interacting with the contract
 const contract = smartweave
   .contract(deployedContracts.loot)
-  .connect('use_wallet')
+  .connect("use_wallet")
   .setEvaluationOptions({
     waitForConfirmation: true,
-  })
+    updateCacheForEachInteraction: false,
+  });
 
-export default contract
+export default contract;
